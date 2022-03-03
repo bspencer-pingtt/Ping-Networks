ab8acfd255805da3ca51a1e2970d0b7ac2c3e3b4
Maya Douglas <mayacrd25@gmail.com>
2022-03-03 07:06:15 -0400

    Add files via upload

---
+++ payroll_reports/report/report_empolyee_td4.py
@@ -0,0 +1,15 @@
+from odoo import api, fields, models, _
+
+
+class DentalAppointmentReport(models.AbstractModel):
+    _name = 'report.payroll_reports.report_employee'
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        docs = self.env['hr.employee'].browse(data['context']['active_ids'])
+        return {
+            'doc_ids': docids,
+            'doc_model': 'hr.employee',
+            'docs': docs,
+            'data': data,
+        }
